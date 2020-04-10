ed5e81b1b87748322a7002eea1a05acf3c1d52ae
Pokerpoke <pokerpoke@qq.com>
2020-04-11 01:40:13 +0800

    Fix match test

--- match_test.py
+++ match_test.py
@@ -1,16 +1,23 @@
-from window_funcs import *
 import cv2
 import time
 import os
 import json
+import getopt
+import sys
+from window_funcs import *
 
-resource_path = "1.png"
-template_path = "2.png"
+resource_path = "./mods/match_test/img/resource.png"
+template_path = "./mods/match_test/img/template.png"
 
 
 def main():
-    # resource = get_screenshot(title)
-    resource = cv2.imread(resource_path)
+    opts, args = getopt.getopt(sys.argv[1:], "t:")
+
+    if args[0] == "screenshot":
+        resource = get_screenshot(title)
+    elif args[0] == "file":
+        resource = cv2.imread(resource_path)
+
     template = cv2.imread(template_path)
 
     match(resource, template, show_result=True)
