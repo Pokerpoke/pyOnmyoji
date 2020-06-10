703dbd00e30397214f0a269382884f7765599093
Jiang Yang <j824544269@gmail.com>
2020-06-10 21:34:04 +0800

    更新百鬼夜行策略 - 现在优先砸概率up、好友概率up、豆子获取了 - 能够优先砸带灯笼的式神了 - 增加了砸完方式的检测

--- mods/bai_gui_ye_xing/process.py
+++ mods/bai_gui_ye_xing/process.py
@@ -5,10 +5,15 @@ import onmyoji.utils as u
 import onmyoji.onmyoji_funcs as o
 
 
+def img_path(img_name):
+    path = os.path.join(__file__, "..", "img", img_name+".png")
+    return path
+
+
 def main_process(times=1):
     img_dir = os.path.join(__file__, "..", "img")
 
-    logging.info("Will run for "+str(times)+" times.")
+    logging.info("将运行："+str(times)+"次")
 
     INVITED_MAX = False
     INVITED = False
@@ -21,27 +26,28 @@ def main_process(times=1):
             if not INVITED_MAX:
                 INVITED_MAX = False
 
-                logging.info("运行:" + str(i + 1) + "次.")
+                logging.info("第" + str(i + 1) + "次")
                 # 邀请好友
-                logging.info("Search for yao_qing_hao_you.png.")
-                p = u.wait_until(os.path.join(img_dir, "yao_qing_hao_you.png"))
+                logging.info("匹配 yao_qing_hao_you.png.")
+                p = u.wait_until(img_path("yao_qing_hao_you"))
                 u.random_sleep(1, 0.5)
                 u.random_click(p, 10)
                 u.random_sleep(1, 0.5)
             # 选择好友
-            logging.info("Search for xuan_ze_hao_you.png.")
+            logging.info("匹配 xuan_ze_hao_you.png.")
             p = u.wait_until(os.path.join(
                 img_dir, "xuan_ze_hao_you.png"),
                 thresold=0.5)
             u.random_sleep(1, 0.5)
             u.random_click(p, 5)
             try:
+                # 邀请次数上限
                 logging.info("Search for yao_qing_ci_shu_shang_xian.png.")
-                p = u.wait_until(os.path.join(
-                    img_dir, "yao_qing_ci_shu_shang_xian.png"), timeout=1, interval=0.1, notify=False)
-                if p != None:
-                    p = u.exists(os.path.join(
-                        img_dir, "guan_bi_xuan_ze_hao_you.png"))
+                p = u.wait_until(img_path("yao_qing_ci_shu_shang_xian"),
+                                 timeout=2, interval=0.1, notify=False)
+                if p is not None:
+                    p = u.exists(img_path("guan_bi_xuan_ze_hao_you"))
+                    p = u.offset_position(p, (0, 200))
                     u.random_sleep(1, 0.5)
                     u.random_click(p, 5)
                     u.random_sleep(1, 0.5)
@@ -52,24 +58,44 @@ def main_process(times=1):
                 pass
             # 进入
             logging.info("Search for jin_ru.png.")
-            p = u.wait_until(os.path.join(img_dir, "jin_ru.png"))
+            p = u.wait_until(img_path("jin_ru"))
             u.random_sleep(1, 0.5)
             u.random_click(p, 20)
+            try:
+                # 邀请次数上限
+                logging.info("Search for yao_qing_ci_shu_shang_xian.png.")
+                p = u.wait_until(img_path("yao_qing_ci_shu_shang_xian"),
+                                 timeout=2, interval=0.1, notify=False)
+                if p is not None:
+                    p = u.exists(img_path("guan_bi_xuan_ze_hao_you"))
+                    p = u.offset_position(p, (0, 200))
+                    u.random_sleep(1, 0.5)
+                    u.random_click(p, 5)
+                    u.random_sleep(1, 0.5)
+                    INVITED_MAX = True
+                    INVITED = True
+                    i = i - 1
+                    continue
+            except TimeoutError:
+                pass
             u.random_sleep(1, 0.5)
             # 选择鬼王，选择中间的
             logging.info("Search for xuan_ze_gui_wang.png.")
             try:
-                p = u.wait_until(os.path.join(
-                    img_dir, "xuan_ze_gui_wang.png"), timeout=10)
+                p = u.wait_until(img_path("xuan_ze_gui_wang"), timeout=10)
             except TimeoutError:
                 i = i - 1
                 INVITED = True
                 continue
+            # 点击中间的鬼王
+            u.random_sleep(1, 0.5)
+            p = u.offset_position(p, (0, 300))
+            u.random_click(p, 20)
+            u.random_sleep(2, 0.5)
         else:
             # 邀请
             logging.info("Search for qu_xiao_xuan_ze_hao_you.png.")
-            p = u.wait_until(os.path.join(
-                img_dir, "qu_xiao_xuan_ze_hao_you.png"))
+            p = u.wait_until(img_path("qu_xiao_xuan_ze_hao_you"))
             u.random_sleep(1, 0.5)
             u.random_click(p, 20)
             u.random_sleep(1, 0.5)
@@ -77,33 +103,62 @@ def main_process(times=1):
             INVITED = False
             continue
 
-        u.random_sleep(1, 0.5)
-        p = u.offset_position(p, (0, 300))
-        u.random_click(p, 20)
-        u.random_sleep(2, 0.5)
         # 开始
         logging.info("Search for kai_shi.png.")
-        p = u.wait_until(os.path.join(img_dir, "kai_shi.png"))
+        p = u.wait_until(img_path("kai_shi"))
         u.random_sleep(1, 0.5)
         u.random_click(p, 20)
         u.random_sleep(3, 0.5)
         # 砸豆子
-        logging.info("Start piu piu piu.")
+        logging.info("开始砸豆子。")
         begin_time = datetime.now()
         while (True):
             # 判断是否完成，前面一直点一定概率直接跳过分享界面了
-            p = u.exists(os.path.join(img_dir, "feng_xiang.png"))
+            p = u.exists(img_path("feng_xiang"))
             if p != None:
                 u.random_sleep(1, 0.2)
                 p = u.position_relative(0.95, 0.55)
                 u.random_click(p, 20)
                 u.random_sleep(0.5, 0.2)
+
+                logging.info("砸完了")
                 break
-            p = u.position_relative(0.8, 0.55)
-            u.random_click(p, 20)
-            u.random_sleep(0.5, 0.2)
+            # 存在豆子获取
+            p = u.exists(img_path("dou_zi_huo_qu"))
+            if p is not None:
+                p = u.offset_position(p, (-30, -20))
+                u.random_click(p, 5)
+                u.random_sleep(0.5, 0.2)
+            # 概率up
+            p = u.exists(img_path("gai_lv_up"))
+            if p is not None:
+                p = u.offset_position(p, (-30, -20))
+                u.random_click(p, 5)
+                u.random_sleep(0.5, 0.2)
+            # 好友概率up
+            p = u.exists(img_path("hao_you_gai_lv_up"))
+            if p is not None:
+                p = u.offset_position(p, (-30, -20))
+                u.random_click(p, 5)
+                u.random_sleep(0.5, 0.2)
+            # 优先砸带灯笼的
+            p = u.exists(img_path("deng_long"))
+            if p is not None:
+                # 大概由于识别速度慢，要增加一定偏移才能砸到正下方
+                p = u.offset_position(p, (-100, 100))
+                u.random_click(p, 20)
+                u.random_sleep(0.5, 0.2)
+            else:
+                p = u.position_relative(0.8, 0.55)
+                u.random_click(p, 20)
+                u.random_sleep(0.5, 0.2)
+            # 分享界面出现以后一定概率直接跳过，检测到进入即视为当次完成
+            if u.exists(img_path("jin_ru")):
+                logging.info("砸完了")
+                break
             # 超时退出
-            if (datetime.now() - begin_time).seconds >= 80:
+            if (datetime.now() - begin_time).seconds >= 100:
+                logging.info("超时退出")
                 break
 
         u.random_sleep(3, 0.3)
